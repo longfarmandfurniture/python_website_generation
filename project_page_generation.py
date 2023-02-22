803a34275d70d1430c6a9f5c2895ce9db05f7bc3
longfarmandfurniture <longfarmandfurniture@outlook.com>
2023-02-21 20:24:50 -0700

    Initial data

--- project_page_generation.py
+++ project_page_generation.py
@@ -9,6 +9,25 @@ def main():
         print(f"{input_directory} is not a directory.")
         exit(0)
 
+#Check for images object 
+    json_file_data = None
+    json_file_name = os.path.join(input_directory, "images.json")
+    
+    #Output filename
+    html_filename = None
+
+    #Tab title
+    short_title = None
+
+    #Header title
+    long_title = None
+
+    if os.path.exists(json_file_name):
+        if os.path.isfile(json_file_name):
+            file = open(json_file_name)
+            json_file_data = json.load(file)
+            if "html_filename" in json_file_data:
+                html_filename = json_file_data["html_filename"]
 
 
 
